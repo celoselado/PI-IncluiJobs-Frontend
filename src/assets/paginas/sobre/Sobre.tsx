ca29f8da6320bc5dbbe0ee434b85c75122ea757f
VinnyPC <vinisilvax3@gmail.com>
2023-05-22 22:05:15 -0300

    inicio da estilização da pagina sobre

--- src/assets/paginas/sobre/Sobre.tsx
+++ src/assets/paginas/sobre/Sobre.tsx
@@ -1,31 +1,35 @@
-import { Grid } from "@material-ui/core";
+import { Box, Grid } from "@material-ui/core";
 import "./Sobre.css"
 function Sobre() {
   return (
     <>
       <Grid container spacing={1}>
-        <Grid item xs={8} className="sobreTexto">
-          <h1>Sobre</h1>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. At
-            accusantium officiis quas sunt labore, voluptatum perferendis.
-            Numquam obcaecati ipsa aspernatur voluptatem velit ut quo.
-            Perspiciatis harum laudantium odio minus eaque. Lorem ipsum dolor
-            sit, amet consectetur adipisicing elit. At accusantium officiis quas
-            sunt labore, voluptatum perferendis. Numquam obcaecati ipsa
-            aspernatur voluptatem velit ut quo. Perspiciatis harum laudantium
-            odio minus eaque. Lorem ipsum dolor sit, amet consectetur
-            adipisicing elit. At accusantium officiis quas sunt labore,
-            voluptatum perferendis. Numquam obcaecati ipsa aspernatur voluptatem
-            velit ut quo. Perspiciatis harum laudantium odio minus eaque. Lorem
-            ipsum dolor sit, amet consectetur adipisicing elit. At accusantium
-            officiis quas sunt labore, voluptatum perferendis. Numquam obcaecati
-            ipsa aspernatur voluptatem velit ut quo. Perspiciatis harum
-            laudantium odio minus eaque.
-          </p>
+        <Grid item xs={8}>
+          <Box className="sobreTexto">
+            <h1>Sobre</h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At
+              accusantium officiis quas sunt labore, voluptatum perferendis.
+              Numquam obcaecati ipsa aspernatur voluptatem velit ut quo.
+              Perspiciatis harum laudantium odio minus eaque. Lorem ipsum dolor
+              sit, amet consectetur adipisicing elit. At accusantium officiis
+              quas sunt labore, voluptatum perferendis. Numquam obcaecati ipsa
+              aspernatur voluptatem velit ut quo. Perspiciatis harum laudantium
+              odio minus eaque. Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. At accusantium officiis quas sunt labore,
+              voluptatum perferendis. Numquam obcaecati ipsa aspernatur
+              voluptatem velit ut quo. Perspiciatis harum laudantium odio minus
+              eaque. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              At accusantium officiis quas sunt labore, voluptatum perferendis.
+              Numquam obcaecati ipsa aspernatur voluptatem velit ut quo.
+              Perspiciatis harum laudantium odio minus eaque.
+            </p>
+          </Box>
         </Grid>
+
         <Grid item xs={3}>
-          <img className="sobreImagem"
+          <img
+            className="sobreImagem"
             src="/src/images/background-sobre.png"
           ></img>
         </Grid>
