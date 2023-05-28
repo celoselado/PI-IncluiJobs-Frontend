f9baaad0ba4be8796bf11f37e6b8f6d93ee58cf7
VinnyPC <vinisilvax3@gmail.com>
2023-05-27 22:34:26 -0300

    estilização da pagina sobre, componentes de apresentação e descrição da equipe

--- src/assets/components/temas/listaTema/ListaTema.tsx
+++ src/assets/components/temas/listaTema/ListaTema.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { busca } from "../../../../service/service";
-import { Box, Button, Card, CardActions, CardContent, Typography } from "@material-ui/core";
+import { Box, Button, Card, CardActions, CardContent, Grid, Typography } from "@material-ui/core";
 import Tema from "../../../../model/Tema";
 import { TokenState } from "../../../../store/tokens/tokensReducer";
 import { addToken } from "../../../../store/tokens/actions";
@@ -46,57 +46,61 @@ function ListaTema() {
     }, [])
 
     return (
-        <>
-            {temas.map((tema) => (
-                <Box m={2}>
-                    <Card variant="outlined">
-                        <CardContent>
-                            <Typography color="textSecondary" gutterBottom>
-                                Tema
-                            </Typography>
-                            <Typography variant="h5" component="h2">
-                                {tema.descricao}
-                            </Typography>
-                            <Typography color="textSecondary" gutterBottom>
-                                Grupo
-                            </Typography>
-                            <Typography variant="h5" component="h2">
-                                {tema.grupo}
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Box display="flex" justifyContent="center" mb={1.5}>
-                                <Link
-                                    to={`/cadastrarTemas/${tema.id}`}
-                                    className="text-decorator-none"
-                                >
-                                    <Box mx={1}>
-                                        <Button
-                                            variant="contained"
-                                            className="marginLeft"
-                                            size="small"
-                                            color="primary"
-                                        >
-                                            atualizar
-                                        </Button>
-                                    </Box>
-                                </Link>
-                                <Link
-                                    to={`/deletarTema/${tema.id}`}
-                                    className="text-decorator-none"
-                                >
-                                    <Box mx={1}>
-                                        <Button variant="contained" size="small" color="secondary">
-                                            deletar
-                                        </Button>
-                                    </Box>
-                                </Link>
-                            </Box>
-                        </CardActions>
-                    </Card>
+      <>
+        {temas.map((tema) => (
+          <Box m={2}>
+            <Card variant="outlined">
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                  Tema
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {tema.descricao}
+                </Typography>
+                <Typography color="textSecondary" gutterBottom>
+                  Grupo
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {tema.grupo}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Box display="flex" justifyContent="center" mb={1.5}>
+                  <Link
+                    to={`/cadastrarTemas/${tema.id}`}
+                    className="text-decorator-none"
+                  >
+                    <Box mx={1}>
+                      <Button
+                        variant="contained"
+                        className="marginLeft"
+                        size="small"
+                        color="primary"
+                      >
+                        atualizar
+                      </Button>
+                    </Box>
+                  </Link>
+                  <Link
+                    to={`/deletarTema/${tema.id}`}
+                    className="text-decorator-none"
+                  >
+                    <Box mx={1}>
+                      <Button
+                        variant="contained"
+                        size="small"
+                        color="secondary"
+                      >
+                        deletar
+                      </Button>
+                    </Box>
+                  </Link>
                 </Box>
-            ))}
-        </>
+              </CardActions>
+            </Card>
+          </Box>
+        ))}
+      </>
     );
 }
 
--- src/assets/paginas/sobre/Sobre.tsx
+++ src/assets/paginas/sobre/Sobre.tsx
@@ -5,43 +5,173 @@ import { Stack } from "@mui/material";
 import Navbar from "../../components/estaticos/navbar/Navbar";
 
 function Sobre() {
-  
   return (
     <>
-      <Grid container spacing={1}>
-        <Grid item xs={8}>
-          <Box className="sobreTexto">
-            <h1>Sobre nós</h1>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At
-              accusantium officiis quas sunt labore, voluptatum perferendis.
-              Numquam obcaecati ipsa aspernatur voluptatem velit ut quo.
-              Perspiciatis harum laudantium odio minus eaque. Lorem ipsum dolor
-              sit, amet consectetur adipisicing elit. At accusantium officiis
-              quas sunt labore, voluptatum perferendis. Numquam obcaecati ipsa
-              aspernatur voluptatem velit ut quo. Perspiciatis harum laudantium
-              odio minus eaque. Lorem ipsum dolor sit, amet consectetur
-              adipisicing elit. At accusantium officiis quas sunt labore,
-              voluptatum perferendis. Numquam obcaecati ipsa aspernatur
-              voluptatem velit ut quo. Perspiciatis harum laudantium odio minus
-              eaque. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              At accusantium officiis quas sunt labore, voluptatum perferendis.
-              Numquam obcaecati ipsa aspernatur voluptatem velit ut quo.
-              Perspiciatis harum laudantium odio minus eaque.
-            </p>
-          </Box>
-
-          
-
-        </Grid>
-
-        <Grid item xs={3}>
-          <img
-            className="sobreImagem"
-            src="/src/images/background-sobre.png"
-          ></img>
-        </Grid>
-      </Grid>
+      <Stack direction={"row"}>
+        <Box className="sobreTexto">
+          <h1>Sobre nós</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. At
+            accusantium officiis quas sunt labore, voluptatum perferendis.
+            Numquam obcaecati ipsa aspernatur voluptatem velit ut quo.
+            Perspiciatis harum laudantium odio minus eaque. Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit. At accusantium officiis quas
+            sunt labore, voluptatum perferendis. Numquam obcaecati ipsa
+            aspernatur voluptatem velit ut quo. Perspiciatis harum laudantium
+            odio minus eaque. Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. At accusantium officiis quas sunt labore,
+            voluptatum perferendis. Numquam obcaecati ipsa aspernatur voluptatem
+            velit ut quo. Perspiciatis harum laudantium odio minus eaque. Lorem
+            ipsum dolor sit, amet consectetur adipisicing elit. At accusantium
+            officiis quas sunt labore, voluptatum perferendis. Numquam obcaecati
+            ipsa aspernatur voluptatem velit ut quo. Perspiciatis harum
+            laudantium odio minus eaque.
+          </p>
+        </Box>
+        <Box>
+          <div className="sobreImagem"></div>
+        </Box>
+      </Stack>
+      <h1 style={{marginLeft: "2em"}}>Conheça nossa equipe!</h1>
+      <Stack direction={"row"}>
+        <Box marginX={"auto"}>
+          <Stack direction={"row"} flexWrap={"wrap"}>
+            <div className="person">
+              <div className="container">
+                <div className="container-inner">
+                  <img className="circle" src="*** imagem de fundo ***" />
+                  <img className="img img1" src="** imagem do perfil **" />
+                </div>
+              </div>
+              <div className="divider"></div>
+              <div className="name">Desenvolvedor 1</div>
+              <div className="title">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. At
+                accusantium officiis quas sunt labore, voluptatum perferendis.
+                Numquam obcaecati ipsa aspernatur voluptatem velit ut quo.
+                Perspiciatis harum laudantium odio minus eaque. Lorem ipsum
+                dolor sit, amet consectetur adipisicing elit. At accusantium
+                officiis quas sunt labore, voluptatum perferendis. Numquam
+                obcaecati ipsa aspernatur voluptatem velit ut quo. Perspiciatis
+                harum laudantium odio minus eaque. Lorem ipsum dolor sit, amet
+                consectetur
+              </div>
+            </div>
+            {/* imagem 2* */}
+            <div className="person">
+              <div className="container">
+                <div className="container-inner">
+                  <img className="circle" src="*** imagem de fundo ***" />
+                  <img className="img img2" src="** imagem do perfil **" />
+                </div>
+              </div>
+              <div className="divider"></div>
+              <div className="name">Desenvolvedor 2</div>
+              <div className="title">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. At
+                accusantium officiis quas sunt labore, voluptatum perferendis.
+                Numquam obcaecati ipsa aspernatur voluptatem velit ut quo.
+                Perspiciatis harum laudantium odio minus eaque. Lorem ipsum
+                dolor sit, amet consectetur adipisicing elit. At accusantium
+                officiis quas sunt labore, voluptatum perferendis. Numquam
+                obcaecati ipsa aspernatur voluptatem velit ut quo. Perspiciatis
+                harum laudantium odio minus eaque. Lorem ipsum dolor sit, amet
+                consectetur
+              </div>
+            </div>
+            {/* imagem 3* */}
+            <div className="person">
+              <div className="container">
+                <div className="container-inner">
+                  <img className="circle" src="*** imagem de fundo ***" />
+                  <img className="img img3" src="** imagem do perfil **" />
+                </div>
+              </div>
+              <div className="divider"></div>
+              <div className="name">Desenvolvedor 3</div>
+              <div className="title">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. At
+                accusantium officiis quas sunt labore, voluptatum perferendis.
+                Numquam obcaecati ipsa aspernatur voluptatem velit ut quo.
+                Perspiciatis harum laudantium odio minus eaque. Lorem ipsum
+                dolor sit, amet consectetur adipisicing elit. At accusantium
+                officiis quas sunt labore, voluptatum perferendis. Numquam
+                obcaecati ipsa aspernatur voluptatem velit ut quo. Perspiciatis
+                harum laudantium odio minus eaque. Lorem ipsum dolor sit, amet
+                consectetur
+              </div>
+            </div>
+          </Stack>
+          <Stack direction={"row"} flexWrap={"wrap"} marginBottom={10}>
+            {/* imagem 4* */}
+            <div className="person">
+              <div className="container">
+                <div className="container-inner">
+                  <img className="circle" src="*** imagem de fundo ***" />
+                  <img className="img img3" src="** imagem do perfil **" />
+                </div>
+              </div>
+              <div className="divider"></div>
+              <div className="name">Desenvolvedor 4</div>
+              <div className="title">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. At
+                accusantium officiis quas sunt labore, voluptatum perferendis.
+                Numquam obcaecati ipsa aspernatur voluptatem velit ut quo.
+                Perspiciatis harum laudantium odio minus eaque. Lorem ipsum
+                dolor sit, amet consectetur adipisicing elit. At accusantium
+                officiis quas sunt labore, voluptatum perferendis. Numquam
+                obcaecati ipsa aspernatur voluptatem velit ut quo. Perspiciatis
+                harum laudantium odio minus eaque. Lorem ipsum dolor sit, amet
+                consectetur
+              </div>
+            </div>
+            {/* imagem 5* */}
+            <div className="person">
+              <div className="container">
+                <div className="container-inner">
+                  <img className="circle" src="*** imagem de fundo ***" />
+                  <img className="img img3" src="** imagem do perfil **" />
+                </div>
+              </div>
+              <div className="divider"></div>
+              <div className="name">Desenvolvedor 5</div>
+              <div className="title">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. At
+                accusantium officiis quas sunt labore, voluptatum perferendis.
+                Numquam obcaecati ipsa aspernatur voluptatem velit ut quo.
+                Perspiciatis harum laudantium odio minus eaque. Lorem ipsum
+                dolor sit, amet consectetur adipisicing elit. At accusantium
+                officiis quas sunt labore, voluptatum perferendis. Numquam
+                obcaecati ipsa aspernatur voluptatem velit ut quo. Perspiciatis
+                harum laudantium odio minus eaque. Lorem ipsum dolor sit, amet
+                consectetur
+              </div>
+            </div>
+            {/* imagem 6* */}
+            <div className="person">
+              <div className="container">
+                <div className="container-inner">
+                  <img className="circle" src="*** imagem de fundo ***" />
+                  <img className="img img3" src="** imagem do perfil **" />
+                </div>
+              </div>
+              <div className="divider"></div>
+              <div className="name">Desenvolvedor 6</div>
+              <div className="title">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. At
+                accusantium officiis quas sunt labore, voluptatum perferendis.
+                Numquam obcaecati ipsa aspernatur voluptatem velit ut quo.
+                Perspiciatis harum laudantium odio minus eaque. Lorem ipsum
+                dolor sit, amet consectetur adipisicing elit. At accusantium
+                officiis quas sunt labore, voluptatum perferendis. Numquam
+                obcaecati ipsa aspernatur voluptatem velit ut quo. Perspiciatis
+                harum laudantium odio minus eaque. Lorem ipsum dolor sit, amet
+                consectetur
+              </div>
+            </div>
+          </Stack>
+        </Box>
+      </Stack>
     </>
   );
 }
