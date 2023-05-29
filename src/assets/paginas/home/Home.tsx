5533adfecf92723ee81cdf696d1dbe75725d1382
Erenilda Tavares <erenyldajsilva@gmail.com>
2023-05-29 17:34:08 -0300

    atualizando tabpostagens e adicionando error no login

--- src/assets/paginas/home/Home.tsx
+++ src/assets/paginas/home/Home.tsx
@@ -11,41 +11,72 @@ import TabPostagem from "../../components/postagens/tabpostagem/TabPostagem";
 function Home() {
   let navigate = useNavigate();
   const token = useSelector<TokenState, TokenState["tokens"]>(
-      (state) => state.tokens
+    (state) => state.tokens
   );
 
   useEffect(() => {
-      if (token == "") {
-          alert("Você precisa estar logado!")
-          navigate("/login")
-      }
-  }, [token])
+    if (token == "") {
+      alert("Você precisa estar logado!");
+      navigate("/login");
+    }
+  }, [token]);
 
   return (
-      <>
-          <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
-              <Grid alignItems="center" item xs={6}>
-                  <Box paddingX={20} >
-                      <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
-                      <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>Expresse aqui os seus pensamentos e opiniões!</Typography>
-                  </Box>
-                  <Box display="flex" justifyContent="center">
-                      <Box marginRight={1}>
-                          <ModalPostagem />
-                      </Box>
-                      <Link to="/postagens">
-                          <Button variant="outlined" className='postHover botao'>Ver Postagens</Button>
-                      </Link>
-                  </Box>
-              </Grid>
-              <Grid item xs={6} >
-                  <img src="https://img.freepik.com/free-vector/gradient-lo-fi-illustration_23-2149375747.jpg" alt="" width="500px" height="500px" />
-              </Grid>
-              <Grid xs={12} className='postagens'>
-                  <TabPostagem />
-              </Grid>
-          </Grid>
-      </>
+    <>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        className="caixa"
+      >
+        <Grid alignItems="center" item xs={6}>
+          <Box paddingX={20}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              component="h3"
+              align="center"
+              className="titulo"
+            >
+              Seja bem vindo(a)!
+            </Typography>
+            <Typography
+              variant="h5"
+              gutterBottom
+              color="textPrimary"
+              component="h5"
+              align="center"
+              className="titulo"
+            >
+              Expresse aqui os seus pensamentos e opiniões!
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="center">
+            <Box marginRight={1}>
+              <ModalPostagem />
+            </Box>
+            <Link to="/postagens">
+              <Button variant="outlined" className="postHover botao">
+                Ver Postagens
+              </Button>
+            </Link>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            src="https://img.freepik.com/free-vector/gradient-lo-fi-illustration_23-2149375747.jpg"
+            alt=""
+            width="500px"
+            height="500px"
+          />
+        </Grid>
+        <Grid xs={12} className="postagens">
+          <TabPostagem  />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
