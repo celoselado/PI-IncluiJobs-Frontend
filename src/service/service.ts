96b43d6a701f5efd1f200dc626d857215042726a
Erenilda Tavares <erenyldajsilva@gmail.com>
2023-06-15 15:55:52 -0300

    processo de deploy

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const api= axios.create({
-    baseURL: "https://incluijobss.onrender.com"
-})
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+});
 
 export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
     const resposta = await api.post(url,dados)
